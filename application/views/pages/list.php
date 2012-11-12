a8a44af7b94103823b77108b3e0e0f3623e3036d
sagieske <sharongieske@gmail.com>
2012-11-12 05:42:47 -0500

    list.php single recipe works with recipe model

--- application/views/pages/list.php
+++ application/views/pages/list.php
@@ -4,7 +4,7 @@
     }
     else{
         foreach ($recipes as $recipe):
-            echo "<h3> <a href=\"show_recipe/$recipe->recipeID\">
+            echo "<h3> <a href=\"../recipe/show/$recipe->recipeID\">
                 $recipe->name </a> </h3>";
             echo "<p> $recipe->description <br>";
             echo "Takes <b>$recipe->time</b> minutes. 
