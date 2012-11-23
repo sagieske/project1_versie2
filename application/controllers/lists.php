c19afbed02573121dd1ded756c8ebdacd94cd5f4
sagieske <sharongieske@gmail.com>
2012-11-23 12:10:10 -0500

    page NeedLogin added for favorites and recently viewed without login

--- application/controllers/lists.php
+++ application/controllers/lists.php
@@ -22,33 +22,40 @@ class Lists extends CI_Controller {
     }
     
     public function recently_viewed() {
+        //Header
+        $data['title'] = 'Recently Viewed';
+        $this->load->view('templates/header', $data);
+        
+        //Body
         $li = $this->session->userdata('logged_in');
         if ($li) {
             $un = $this->session->userdata('username');
-            
-            // At this point, delete non-recent data?
-            // $this->db->delete('mytable', array('username' => $un, 'time' => 'nonrecent?')); 
-            
             $data['recipes'] = $this->User_model->get_recently_viewed($un);
+            $this->load->view('pages/list', $data);
+        }else{
+            $this->load->view('pages/needlogin', $data);
         }
-        $data['title'] = 'Recently Viewed';
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('pages/list', $data);
+
         $this->load->view('templates/footer', $data);    
     }
     
     public function show_favorites() {
-        $li = $this->session->userdata('logged_in');
+
+        $data['title'] = 'Favorites';;
+        
+        $this->load->view('templates/header', $data);
+        $li = $this->session->userdata('logged_in');        
         if ($li) {
             $un = $this->session->userdata('username');
             
             $data['recipes'] = $this->User_model->get_favorites($un);
+            $this->load->view('pages/list', $data);
+        }else{
+            $this->load->view('pages/needlogin', $data);
         }
-        $data['title'] = 'Recently Viewed';;
-        
-        $this->load->view('templates/header', $data);
-        $this->load->view('pages/list', $data);
+
+
         $this->load->view('templates/footer', $data);    
     }
     
---
+++ application/views/pages/needlogin.php
@@ -0,0 +1,4 @@
+<?
+    echo '<h3>You need to login to use this page.<h3>
+    <h4>Login at <a href="../../login">homepage</a><h4>'
+?>
